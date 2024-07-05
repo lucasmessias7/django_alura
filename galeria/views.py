d18ee5e9b906c056973da3552af11ad9c4853c06
Lucas Messias Alves <luquinhasmessias7@gmail.com>
2024-07-05 12:19:59 -0300

    introducao_ORM

--- galeria/views.py
+++ galeria/views.py
@@ -4,5 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+dados = {
+    1: {'nome': 'Nebulosa de Carina',
+    'legenda': 'webbtelescope.org / NASA / James Webb',
+    },
+
+    2: {'nome': 'Galáxia NGC 1079',
+    'legenda': 'nasa.org / NASA / Hubble',
+    }
+}
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', {'cards': dados})
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
